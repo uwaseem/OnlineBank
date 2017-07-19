20fc598d3eef3ee83edc6c6c150bfb3a50415e1b
Ubaidillah Waseem <ubaidillah@iflix.com>
2017-07-19 22:38:37 +0800

    Updated schema and added post and delete

--- db/users.js
+++ db/users.js
@@ -1,7 +1,9 @@
 import Mongoose from 'mongoose'
 
 const userSchema = Mongoose.Schema({
-  name: String
+  username: String,
+  firstName: String,
+  lastName: String
 })
 
 module.exports = Mongoose.model('Users', userSchema)
