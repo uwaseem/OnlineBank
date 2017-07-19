70360ddc22e921f28f826c86c5ac33a799643892
Ubaidillah Waseem <ubaidillah@iflix.com>
2017-07-19 20:09:17 +0800

    Import test routes and initialise mongo db via mongoose module

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import BodyParser from 'body-parser'
 import Express from 'express'
 
+import mongoDB from '../db/index'
 import routesAccount from '../routes/account'
+import routesUsers from '../routes/users'
 
 export default async function () {
-  const app = Express()
+  mongoDB()
 
+  const app = Express()
   app.use(BodyParser.json())
 
   routesAccount(app)
+  routesUsers(app)
   app.get('/ping', (req, res) => res.status(200).json({ message: 'pong' }))
   app.all('*', (req, res) => res.sendStatus(404))
 
