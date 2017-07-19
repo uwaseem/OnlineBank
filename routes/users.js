2b11ec13e958cb8a09e3ab3de450e2131ce7b3cf
Ubaidillah Waseem <u.waseem24@gmail.com>
2017-07-19 22:40:26 +0800

    Updated schema and added post and delete (#4)

--- routes/users.js
+++ routes/users.js
@@ -6,30 +6,51 @@ export default function (app) {
   app.get('/users', async (req, res) => {
     try {
       const users = await Users.find()
-      if (users.length < 1) {
-        res.status(204)
-      }
-
-      res.status(200).send(users)
+      res.status(200).json(users)
     } catch (error) {
       console.error('Error while retrieving all users', error)
       res.sendStatus(500)
     }
   })
 
-  app.get('/user/name/:name', async (req, res) => {
-    const { name } = req.params
+  app.get('/user/username/:username', async (req, res) => {
+    const { username } = req.params
 
     try {
-      const user = await Users.findOne({ name })
+      const user = await Users.findOne({ username })
+      res.status(200).json(user)
+    } catch (error) {
+      console.error(`Error while retrieving user ${username}`, error)
+      res.sendStatus(500)
+    }
+  })
 
-      if (!user) {
-        res.status(204)
+  app.post('/user', async(req, res) => {
+    const userInfo = req.body
+
+    try {
+      let user = await Users.findOne({ username: userInfo.username })
+
+      if (user) {
+        return res.status(400).json({ message: 'username already exist' })
       }
 
-      res.status(200).send(user)
+      user = await Users.create(userInfo)
+      res.status(200).json(user)
     } catch (error) {
-      console.error('Error while retrieving user', error)
+      console.error('Error while creating user', error)
+      res.sendStatus(500)
+    }
+  })
+
+  app.delete('/user/username/:username', async (req, res) => {
+    const { username } = req.params
+
+    try {
+      const { result } = await Users.remove({ username })
+      res.status(200).json(result)
+    } catch (error) {
+      console.error(`Error while deleting user ${username}`, error)
       res.sendStatus(500)
     }
   })
