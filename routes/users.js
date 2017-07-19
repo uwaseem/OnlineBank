6bb5c79e28be7408e82e11ad2b20ecfaedda6348
Ubaidillah Waseem <u.waseem24@gmail.com>
2017-07-19 21:10:15 +0800

    Get users (#3)
    * Cleaning up the connecting db module
    
    * Removing unneeded asyn generator pachkage
    
    * Get users all and by name
    
    * Add some space
    
    * Add nvmrc file, easy to track versoin

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,34 @@ import Mongoose from 'mongoose'
 export default function (app) {
   const Users = Mongoose.model('Users')
 
-  app.get('/users', (req, res) => {
-    Users.findOne({ name: 'waseem' }, (error, response) => {
-      if (error) {
-        console.error('Error while retrieving user', error)
-        res.status(500)
+  app.get('/users', async (req, res) => {
+    try {
+      const users = await Users.find()
+      if (users.length < 1) {
+        res.status(204)
       }
 
-      res.status(200).json(response)
-    })
+      res.status(200).send(users)
+    } catch (error) {
+      console.error('Error while retrieving all users', error)
+      res.sendStatus(500)
+    }
+  })
+
+  app.get('/user/name/:name', async (req, res) => {
+    const { name } = req.params
+
+    try {
+      const user = await Users.findOne({ name })
+
+      if (!user) {
+        res.status(204)
+      }
+
+      res.status(200).send(user)
+    } catch (error) {
+      console.error('Error while retrieving user', error)
+      res.sendStatus(500)
+    }
   })
 }
