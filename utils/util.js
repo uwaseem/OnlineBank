177875318ab880c27fa9c4218cb7567d4825cb0a
Ubaidillah Waseem <ubaidillah@iflix.com>
2017-07-22 18:51:01 +0800

    Update readme and add unit test

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,12 @@
 function createReturnObject (code, success, message, error) {
+  if (success !== true && success !== false) {
+    return {
+      code: 500,
+      success: false,
+      message: `${success} is not a valid value for success`
+    }
+  }
+
   if (error) {
     console.error(message, error)
   }
